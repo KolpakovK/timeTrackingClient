ecfca69a7fd0f88abaecee174d54764bcac7d7e2
Kyrylo <kolpakov.k99@gmail.com>
2024-04-06 20:05:01 +0200

    Proxy edits v6

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,13 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://207.154.193.21:3000",
+        target: "http://207.154.193.21",
         changeOrigin: true,
         secure: false,      
         ws: true,
       },
     },
+    port: 3000,
   },
   plugins: [vue()],
 })
