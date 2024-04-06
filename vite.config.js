9e69413ac0c5672ffe80bb5b01813f2b67988c2c
Kyrylo <kolpakov.k99@gmail.com>
2024-04-06 19:33:39 +0200

    Proxy edits

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://207.154.193.21:3000",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [vue()],
 })
